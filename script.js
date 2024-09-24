d9b9d2906933ad92b836be689d2a57f2dd2145a8
Luka <luka.tarkhnishvili2002@gmail.com>
2024-09-24 16:56:05 +0400

    second variant

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const cards = document.querySelectorAll(".card");
   cards.forEach((card) => {
     const bookButton = card.querySelector(".book-button");
-    const priceButton = card.querySelector(".price-button");
-    const image = card.querySelector(".image");
-    const cardContent = card.querySelector(".card-header");
     const payment = card.querySelector(".payment");
     let isReserved = false;
 
@@ -14,22 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
       card.addEventListener("mouseleave", () => {
         if (isReserved) {
-          cardContent.classList.add("reserved");
+          card.classList.add("reserved");
           payment.style.display = "block";
-          priceButton.style.display = "none";
-          image.style.opacity = "0.6";
-          card.classList.add("no-background");
         }
       });
     });
 
-    cardContent.addEventListener("click", () => {
+    card.addEventListener("click", () => {
       if (card.classList.contains("reserved")) {
         card.classList.remove("reserved");
-        payment.style.display = "none";
-        priceButton.style.display = "block";
-        image.style.opacity = "1";
-        card.classList.remove("no-background");
+        payment.style.display = "block";
 
         isReserved = false;
       }
